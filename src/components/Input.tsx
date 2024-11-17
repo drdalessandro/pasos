1b425d03a8d45b54f682078b9936a1cd4a04f099
Alejandro Sergio D'Alessandro <admin@epa-bienestar.com>
2024-11-17 17:42:50 -0300

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -19,7 +19,7 @@ export default function Input({ type = 'text', value, handleChange, name, placeh
         onChange={(e) => {
           handleChange(name, e.target.value);
         }}
-        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-emerald-700 focus:ring-emerald-700 sm:text-sm"
+        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-700 focus:ring-blue-700 sm:text-sm"
         placeholder={placeholder}
       />
     </div>
