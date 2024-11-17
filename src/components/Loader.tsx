ddc6033e6cf7e94e6d4aa61a87308d8d382ad4f3
Alejandro Sergio D'Alessandro <admin@epa-bienestar.com>
2024-11-17 17:43:36 -0300

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ const Loader = (): JSX.Element => {
     <div className="flex h-full w-full items-center justify-center">
       <div className="relative">
         <div className="absolute h-10 w-10 rounded-full border-4 border-solid border-gray-100" />
-        <div className="absolute h-10 w-10 animate-spin rounded-full border-t-4 border-solid border-green-600" />
+        <div className="absolute h-10 w-10 animate-spin rounded-full border-t-4 border-solid border-blue-600" />
       </div>
     </div>
   );
